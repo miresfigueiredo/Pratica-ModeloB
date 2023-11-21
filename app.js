0f4f35fe8276ca3930ef3212b18470ac31a27aaf
miresfigueiredo <131209919+miresfigueiredo@users.noreply.github.com>
2023-11-21 20:50:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ function hide(){
 image.addEventListener('click', show);
 close.addEventListener('click', hide);
 
-function test(e) {
+
+document.getElementById("buscar-github").addEventListener("click", function() {
     const username = document.getElementById("usuario-github").value;
     const url = `https://api.github.com/users/${username}`;
 
@@ -30,23 +31,17 @@ function test(e) {
         .then(data => {
             document.getElementById("nome-usuario").textContent = data.name || data.login;
             document.getElementById("bio-usuario").textContent = data.bio || 'Bio não disponível';
-            document.getElementById("img-usuario").style.backgroundImage=`url(${data.avatar_url})`;
+            document.getElementById("img-redonda").style.backgroundImage=`url(${data.avatar_url})`;
             document.getElementById("imagem-usuario").src = data.avatar_url;
             document.getElementById("imagem-usuario").alt = `Imagem de ${data.login}`;
             document.getElementById("seguindo").textContent = data.following;
             document.getElementById("seguidores").textContent = data.followers;
-            document.getElementById("posts").textContent = data.public_repos;
+            document.getElementById("postagens").textContent = data.public_repos;
             document.getElementById("location").textContent = data.location;
             document.getElementById("github-usuario").href = data.html_url;
         })
         .catch(error => {
             console.error('Erro:', error);
-            alert('Erro ao buscar dados!')
+            alert('Usuário não encontrado!')
         });
-}
-
-document.getElementById("buscar-github").addEventListener("click", test);
-
-document.getElementById("usuario-github").addEventListener("keyup",function(e){
-    if (e.keyCode===13) test(e)
 });
